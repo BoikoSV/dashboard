984fa220faa73adfa8703b342a9a5fd21e48a5ac
BoikoSV <boikosmail2@gmail.com>
2021-10-05 12:19:05 +0300

    Create a basic database structure

---
+++ database/migrations/2021_10_05_085903_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->text('main_content');
+            $table->string('post_img')->nullable();
+            $table->integer('user_id')->unsigned();
+            $table->boolean('is_publish')->default(0);
+            $table->boolean('is_verify')->default(0);
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
