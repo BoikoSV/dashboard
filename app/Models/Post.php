7c7b20b84603f0a5005a7be91e34176e179f253c
BoikoSV <boikosmail2@gmail.com>
2021-10-05 15:28:04 +0300

    Created links between models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function tags(){
+        return $this->belongsToMany(
+            Tag::class,
+            'post_tag',
+            'post_id',
+            'tag_id'
+        );
+    }
 }
