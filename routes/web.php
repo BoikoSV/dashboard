6ac023e19f0022664167768e88b08d79d36a8f11
BoikoSV <boikosmail2@gmail.com>
2021-10-07 16:30:05 +0300

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\TagController;
 
 
 
-Route::get('/', function () {
-    return view('pages.main');
-});
+Route::resource('users', UserController::class);
+Route::resource('posts', PostController::class);
+Route::resource('categories', CategoryController::class);
+Route::resource('comments', CommentController::class);
+Route::resource('roles', RoleController::class);
+Route::resource('tags', TagController::class);
+
